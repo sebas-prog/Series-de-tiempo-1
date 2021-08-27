987502211fcc3009e60d058a2d715c4d0e84ef32
sebas-prog <sebas12.ssp@gmail.com>
2021-08-27 14:50:39 -0500

    Update Series de tiempo.R

--- R/Series de tiempo.R
+++ R/Series de tiempo.R
@@ -1,2 +1,16 @@
 # Modelos de series de tiempo 
 
+
+# Es ta funcion permite cargar y descargar los paquetes de forma automatica
+
+ipak <- function(pkg){
+  new.pkg <- pkg[!(pkg %in% installed.packages()[, "Package"])]
+  if (length(new.pkg)) 
+    install.packages(new.pkg, dependencies = TRUE)
+  sapply(pkg, require, character.only = TRUE)
+}
+
+packages <- c("forecast","TSstudio","tseries","highcharter","TTR")
+ipak(packages)
+
+
